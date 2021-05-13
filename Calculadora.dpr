111b8f4773037a2ae45759f79901146a91064e1b
Kathleen <katy.krunitzki@hotmail.com>
2021-05-12 23:29:56 -0300

    Commit inicial

---
+++ Calculadora.dpr
@@ -0,0 +1,16 @@
+program Calculadora;
+
+
+
+uses
+  Vcl.Forms,
+  teste in '..\teste.pas' {calculator};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(Tcalculator, calculator);
+  Application.Run;
+end.
